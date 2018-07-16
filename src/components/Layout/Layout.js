b2c908c51f39609898403f810b25e823ccee70f5
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-16 09:58:45 +0200

    Dodanie stylow do komponentu Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Wrap from '../../hoc/Wrap'
+import classes from './Layout.css'
 
 const layout =(props) => (
 	<Wrap>
 		<div>Toolbar,SideDrawer, Blackdrop</div>
-		<main>
+		<main className={classes.Content}>
 			{props.children}
 		</main>
 	</Wrap>
