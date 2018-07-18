384ea8c9804f404e6f5ef78c2a4965765c953185
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-18 16:14:53 +0200

    Powiązanie stanu Burger'a z BuildControls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -6,13 +6,20 @@ const controls = [
 	{label: 'Salad', type: 'salad'},
 	{label: 'Bacon', type: 'bacon'},
 	{label: 'Cheese', type: 'cheese'},
-	{label: 'Meat', type: 'Meat'}
+	{label: 'Meat', type: 'meat'}
 ];
 
 const buildControls = (props) => (
 	<div className={classes.BuildControls}>
+		<p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
 		{controls.map(ctrl => (
-			<BuildControl key={ctrl.label} label = {ctrl.label}/>
+			<BuildControl
+				key={ctrl.label}
+				label = {ctrl.label}
+				added = {() => props.ingredientAdded(ctrl.type)}
+				removed = {() => props.ingredientRemoved(ctrl.type)}
+				disabled = {props.disabled[ctrl.type]}
+			/>
 		))}
 	</div>
 );
