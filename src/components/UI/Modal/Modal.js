66f6946498c6f34a74a18d94a80b6ed413cd5c21
Pl4cek <pl4cek@outlook.com>
2018-07-19 20:51:27 +0200

    Ukrywanie modalnego okna

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import classes from './Modal.css'
+import Wrap from '../../../hoc/Wrap'
+import Backdrop from '../Backdrop/Backdrop'
 
 const modal = (props) => (
-	<div
-		className={classes.Modal}
-		style = {{transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-			opacity: props.show ? '1' : '0'}}>
-		{props.children}
-	</div>
+	<Wrap>
+		<Backdrop show = {props.show} clicked = {props.modalClosed} />
+		<div
+			className={classes.Modal}
+			style = {{transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
+				opacity: props.show ? '1' : '0'}}>
+			{props.children}
+		</div>
+	</Wrap>
 );
 
 export default modal;
