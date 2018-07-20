379a6ce276219a047da4803b23b9ec7885010618
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-20 15:14:26 +0200

    Start

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Wrap from '../../hoc/Wrap'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout =(props) => (
 	<Wrap>
-		<div>Toolbar,SideDrawer, Blackdrop</div>
+		<Toolbar/>
 		<main className={classes.Content}>
 			{props.children}
 		</main>
