2ad36c29c1606b6ddf194a03501636724357a44f
Pl4cek <pl4cek@outlook.com>
2018-07-22 11:06:24 +0200

    Ograniczenie renderowania

--- src/components/Burger/OrderSummery/OrderSummary.js
+++ src/components/Burger/OrderSummery/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Wrap from '../../../hoc/Wrap'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-	const ingredientSummary = Object.keys(props.ingredients)
-		.map(ignKey => {
-			return (
-				<li key ={ignKey}>
-					<span style={{textTransform: 'capitalize'}}>{ignKey}</span>: {props.ingredients[ignKey]}
-				</li>
-			)
-		});
-	return (
-		<Wrap>
-			<h3>Your Order</h3>
-			<p>A Delicious burger with the following ingredients</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType = "Success" clicked = {props.purchaseContinued}>CONTINUE</Button>
-			<Button btnType = "Danger" clicked = {props.purchaseCanceled}>CANCEL</Button>
-		</Wrap>
-	)
+	componentWillUpdate(){
+		console.log("[OrderSummary] WillUpdate")
+	}
+
+	render() {
+		const ingredientSummary = Object.keys(this.props.ingredients)
+			.map(ignKey => {
+				return (
+					<li key ={ignKey}>
+						<span style={{textTransform: 'capitalize'}}>{ignKey}</span>: {this.props.ingredients[ignKey]}
+					</li>
+				)
+			});
+
+		return (
+			<Wrap>
+				<h3>Your Order</h3>
+				<p>A Delicious burger with the following ingredients</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+				<Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+			</Wrap>
+		)
+	}
 };
 
-export default orderSummary;
+export default OrderSummary;
