ad328d9ab6b51a85e38a76a98ec3b029598cfa24
Pl4cek <pl4cek@outlook.com>
2018-07-22 10:18:43 +0200

    Dodanie BackDrop i chowanie bocznej szuflady

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Wrap from '../../hoc/Wrap'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout =(props) => (
-	<Wrap>
-		<Toolbar/>
-		<SideDrawer/>
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</Wrap>
-);
+class Layout extends Component {
 
-export default layout;
+	state = {
+		showSideDrawer: true
+	};
+
+	sideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer: false})
+	};
+
+	render () {
+		return (
+			<Wrap>
+				<Toolbar/>
+				<SideDrawer closed = {this.sideDrawerClosedHandler} open = {this.state.showSideDrawer}/>
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Wrap>
+		)
+	}
+};
+
+export default Layout;
